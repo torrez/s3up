c9b041872174b70849f5ba501df4b2aeee9837c5
Andre Torrez <andre@torrez.org>
2009-07-28 22:03:21 -0700

    send only the file name and the path, not original path

--- s3up.py
+++ s3up.py
@@ -3,7 +3,7 @@
 from boto.s3.connection import S3Connection
 from boto.s3.key import Key
 import settings
-import sys
+import os,sys
 
 if len(sys.argv) < 4:
   print "Usage: <filename> <bucket> <path>"
@@ -12,6 +12,8 @@ if len(sys.argv) < 4:
 s3 = S3Connection(settings.S3_PUBLIC, settings.S3_PRIVATE)
 bucket = s3.get_bucket(sys.argv[2])
 
+(filepath, filename) = os.path.split(sys.argv[1])
+
 k = Key(bucket)
-k.key = "%s/%s" % (sys.argv[3].strip('/'), sys.argv[1])
+k.key = "%s/%s" % (sys.argv[3].strip('/'), filename)
 k.set_contents_from_filename(filename=sys.argv[1], policy='private')
